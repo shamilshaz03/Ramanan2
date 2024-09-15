c30a334a9a971661fd604e5624059162a472d4c7
𝖓𝖆𝖒𝖊𝖑𝖊𝖘𝖘 <strangeakount0@gmail.com>
2024-09-16 02:09:19 +0300

    Update info.py

--- info.py
+++ info.py
@@ -13,8 +13,8 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = environ.get('API_ID', '')
-API_HASH = environ.get('API_HASH', '')
+API_ID = environ.get('API_ID', '21960227')
+API_HASH = environ.get('API_HASH', '459de6e41eef0e35ee8cd9d8812d696f')
 BOT_TOKEN = environ.get('BOT_TOKEN', '')
 
 # Bot settings
@@ -22,13 +22,13 @@ CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = is_enabled((environ.get('USE_CAPTION_FILTER', 'True')), True)
 
 PICS = (environ.get('PICS', 'https://ibb.co/YZ5V3Vw')).split()
-NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/e20b5fdaf217252964202.jpg")
+NOR_IMG = environ.get("NOR_IMG", "https://ibb.co/YZ5V3Vw")
 MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/37c133b738a32483aaf6e.mp4")
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7369035711 6999568783 2143628351 6822307939 7336239235 6884486968 6499050157').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002230015453 -1002441993334').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL')
@@ -42,8 +42,8 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = is_enabled((environ.get("NO_RESULTS_MSG", 'False')), False)
 
 # MongoDB information
-SECONDDB_URI = environ.get('SECONDDB_URI', '')
-DATABASE_URI = environ.get('DATABASE_URI', '')
+SECONDDB_URI = environ.get('SECONDDB_URI', 'mongodb+srv://strangeakount0:zDzPrZ81eJR14siG@anna.wqfc1.mongodb.net/?retryWrites=true&w=majority&appName=anna')
+DATABASE_URI = environ.get('DATABASE_URI', 'mongodb+srv://strangeakount0:4pwSLIt953PLZSVb@anna.uvwtl.mongodb.net/?retryWrites=true&w=majority&appName=anna')
 DATABASE_NAME = environ.get('DATABASE_NAME', 'anna')
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'anna')
 
@@ -62,7 +62,7 @@ PORT = environ.get("PORT", "8080")
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+Ixb1KTjO4-ExMzQ1')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/movieverse_links')
 MSG_ALRT = environ.get('MSG_ALRT', 'Wʜᴀᴛ Aʀᴇ Yᴏᴜ Lᴏᴏᴋɪɴɢ Aᴛ ?')
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002473368072'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'aski_me_bot')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
@@ -76,7 +76,7 @@ LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False")
 SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
 MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
 INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', LOG_CHANNEL))
-FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).split()]
+FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-1002473368072')).split()]
 MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
 PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
